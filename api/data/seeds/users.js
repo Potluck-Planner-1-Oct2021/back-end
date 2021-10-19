680dd7f8b537b722c8a5e3cc935575bfbb769dd1
ehsan2793 <ehsan2793@gmail.com>
2021-10-19 01:34:13 -0700

    test

--- api/data/seeds/users.js
+++ api/data/seeds/users.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
   return knex('users')
     .del()
     .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([{ username: 'ehsan', password: '1234', email: 'ehsan@gmail.com' }]);
+      return knex('users').insert([
+        { username: 'ehsan', password: '1234', email: 'ehsan@gmail.com' },
+      ]);
     });
 };
